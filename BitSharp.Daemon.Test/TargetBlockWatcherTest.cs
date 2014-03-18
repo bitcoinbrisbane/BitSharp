76867fe26fead64f41cb3d37ca7bd6ff371f288b
Paul < pmlyon@hotmail.ca>
2014-03-17 21:42:36 -0300

    missed on checkin

---
+++ BitSharp.Daemon.Test/TargetBlockWatcherTest.cs
@@ -0,0 +1,52 @@
+﻿using BitSharp.Data;
+using BitSharp.Storage;
+using BitSharp.Storage.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Daemon.Test
+{
+    [TestClass]
+    public class TargetBlockWatcherTest
+    {
+        [TestMethod]
+        public void TestSimpleTargetBlock()
+        {
+            // initialize data
+            var chainedBlock0 = new ChainedBlock(blockHash: 0, previousBlockHash: 9999, height: 0, totalWork: 0);
+            var chainedBlock1 = new ChainedBlock(blockHash: 1, previousBlockHash: chainedBlock0.BlockHash, height: 1, totalWork: 1);
+
+            // initialize storage
+            var memoryCacheContext = new CacheContext(new MemoryStorageContext());
+
+            // initialize the target block watcher
+            using (var targetBlockWatcher = new TargetBlockWatcher(memoryCacheContext))
+            {
+                // monitor event firing
+                var eventCount = 0;
+                targetBlockWatcher.OnTargetBlockChanged += () => eventCount++;
+
+                // verify initial state
+                Assert.IsNull(targetBlockWatcher.TargetBlock);
+
+                // add block 0
+                memoryCacheContext.ChainedBlockCache[chainedBlock0.BlockHash] = chainedBlock0;
+                Assert.AreEqual(1, eventCount);
+
+                // verify block 0
+                Assert.AreEqual(chainedBlock0, targetBlockWatcher.TargetBlock);
+
+                // add block 1
+                memoryCacheContext.ChainedBlockCache[chainedBlock1.BlockHash] = chainedBlock1;
+                Assert.AreEqual(2, eventCount);
+
+                // verify block 1
+                Assert.AreEqual(chainedBlock1, targetBlockWatcher.TargetBlock);
+            }
+        }
+    }
+}
