2c7df409c76536458a9ea17af2317d85db01ffd4
Paul < pmlyon@hotmail.ca>
2014-03-25 13:14:41 -0300

    move ChainState into Blockchain project

--- BitSharp.Blockchain/ChainState.cs
+++ BitSharp.Blockchain/ChainState.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using BitSharp.Data;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BitSharp.Data
+namespace BitSharp.Blockchain
 {
     public class ChainState
     {
