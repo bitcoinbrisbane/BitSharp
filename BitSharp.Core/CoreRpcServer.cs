09adb00ca56b02f74baee0d197638cd92d90e959
Paul < pmlyon@hotmail.ca>
2014-04-06 17:53:36 -0300

    stub rpc servers

---
+++ BitSharp.Core/CoreRpcServer.cs
@@ -0,0 +1,26 @@
+﻿using AustinHarris.JsonRpc;
+using BitSharp.Common;
+using BitSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Node
+{
+    public class CoreRpcServer : JsonRpcService
+    {
+        private readonly CoreDaemon coreDaemon;
+
+        public CoreRpcServer(CoreDaemon coreDaemon)
+        {
+            this.coreDaemon = coreDaemon;
+        }
+
+        [JsonRpcMethod("getblock")]
+        public void GetBlock(UInt256 blockHash)
+        {
+        }
+    }
+}
---
+++ BitSharp.Node/NodeRpcServer.cs
@@ -0,0 +1,21 @@
+﻿using AustinHarris.JsonRpc;
+using BitSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Node
+{
+    public class NodeRpcServer : CoreRpcServer
+    {
+        private readonly CoreDaemon coreDaemon;
+
+        public NodeRpcServer(CoreDaemon coreDaemon)
+            : base(coreDaemon)
+        {
+            this.coreDaemon = coreDaemon;
+        }
+    }
+}
