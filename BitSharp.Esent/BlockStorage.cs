7498f856f4d0ec55b46d383edf2f239d1640adb0
Paul < pmlyon@hotmail.ca>
2014-04-10 21:56:16 -0300

    add raw block storage

---
+++ BitSharp.Esent/BlockStorage.cs
@@ -0,0 +1,25 @@
+﻿using BitSharp.Common;
+using BitSharp.Common.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using BitSharp.Core.Domain;
+using BitSharp.Core.Storage;
+using BitSharp.Core;
+
+namespace BitSharp.Esent
+{
+    public class BlockStorage : EsentDataStorage<Block>, IBlockStorage
+    {
+        public BlockStorage(string baseDirectory)
+            : base(baseDirectory, "blocks",
+                block => DataEncoder.EncodeBlock(block),
+                (blockHash, bytes) => DataEncoder.DecodeBlock(bytes))
+        { }
+    }
+}
