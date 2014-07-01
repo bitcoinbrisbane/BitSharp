ccdbe7ac7bb2a20610e60b826dc214308c765e15
Paul < pmlyon@hotmail.ca>
2014-06-30 21:51:24 -0300

    making progress on streaming block elements

--- BitSharp.Core.Test/DataCalculatorNewTest.cs
+++ BitSharp.Core.Test/DataCalculatorNewTest.cs
@@ -21,17 +21,106 @@ namespace BitSharp.Core.Test
 
             var blockHash = (UInt256)0;
 
-            var hash1 = (UInt256)1;
-            var blockElement1 = new BlockElement(blockHash, index: 0, depth: 0, hash: hash1);
+            var blockElement1 = new BlockElement(blockHash, index: 0, depth: 0, hash: 1);
+            var blockElement2 = new BlockElement(blockHash, index: 1, depth: 0, hash: 2);
+            var blockElement3 = new BlockElement(blockHash, index: 2, depth: 0, hash: 3);
+            var blockElement4 = new BlockElement(blockHash, index: 3, depth: 0, hash: 4);
 
-            var hash2 = (UInt256)2;
-            var blockElement2 = new BlockElement(blockHash, index: 1, depth: 0, hash: hash2);
+            var depth1Hash1 = DataCalculatorNew.PairHashes(blockElement1.Hash, blockElement2.Hash);
+            var depth1Hash2 = DataCalculatorNew.PairHashes(blockElement3.Hash, blockElement4.Hash);
+            var merkleRoot = DataCalculatorNew.PairHashes(depth1Hash1, depth1Hash2);
 
-            var merkleRoot = new UInt256(sha256.ComputeDoubleHash(hash1.ToByteArray().Concat(hash2.ToByteArray())));
+            var blockElements = new List<BlockElement> { blockElement1, blockElement2, blockElement3, blockElement4 };
 
-            var blockElements = new List<BlockElement> { blockElement1, blockElement2 };
+            var readElements = DataCalculatorNew.ReadBlockElements(blockHash, merkleRoot, blockElements).ToList();
 
-            var readElements = DataCalculatorNew.ReadBlockElements(merkleRoot, blockElements).ToList();
+            CollectionAssert.AreEqual(blockElements, readElements);
+        }
+
+        [TestMethod]
+        public void TestReadBlockElementsOddPower()
+        {
+            var sha256 = new SHA256Managed();
+
+            var blockHash = (UInt256)0;
+
+            var depth0Hash1 = (UInt256)1;
+            var depth0Hash2 = (UInt256)2;
+            var depth0Hash3 = (UInt256)3;
+
+            var depth1Hash1 = DataCalculatorNew.PairHashes(depth0Hash1, depth0Hash2);
+            var depth1Hash2 = DataCalculatorNew.PairHashes(depth0Hash3, depth0Hash3);
+
+            var merkleRoot = DataCalculatorNew.PairHashes(depth1Hash1, depth1Hash2);
+
+            var blockElements = new List<BlockElement> { 
+                new BlockElement(blockHash, index: 0, depth: 0, hash: depth0Hash1),
+                new BlockElement(blockHash, index: 1, depth: 0, hash: depth0Hash2),
+                new BlockElement(blockHash, index: 2, depth: 0, hash: depth0Hash3),
+            };
+
+            var readElements = DataCalculatorNew.ReadBlockElements(blockHash, merkleRoot, blockElements).ToList();
+
+            CollectionAssert.AreEqual(blockElements, readElements);
+        }
+
+        [TestMethod]
+        public void TestReadBlockElementsPruned()
+        {
+            var sha256 = new SHA256Managed();
+
+            var blockHash = (UInt256)0;
+
+            var depth0Hash1 = (UInt256)1;
+            var depth0Hash2 = (UInt256)2;
+            var depth0Hash3 = (UInt256)3;
+            var depth0Hash4 = (UInt256)4;
+            var depth0Hash5 = (UInt256)5;
+            var depth0Hash6 = (UInt256)6;
+            var depth0Hash7 = (UInt256)7;
+            var depth0Hash8 = (UInt256)8;
+            var depth0Hash9 = (UInt256)9;
+            var depth0Hash10 = (UInt256)10;
+            var depth0Hash11 = (UInt256)11;
+            var depth0Hash12 = (UInt256)12;
+            var depth0Hash13 = (UInt256)13;
+            var depth0Hash14 = (UInt256)14;
+            var depth0Hash15 = (UInt256)15;
+
+            var depth1Hash1 = DataCalculatorNew.PairHashes(depth0Hash1, depth0Hash2);
+            var depth1Hash2 = DataCalculatorNew.PairHashes(depth0Hash3, depth0Hash4);
+            var depth1Hash3 = DataCalculatorNew.PairHashes(depth0Hash5, depth0Hash6);
+            var depth1Hash4 = DataCalculatorNew.PairHashes(depth0Hash7, depth0Hash8);
+            var depth1Hash5 = DataCalculatorNew.PairHashes(depth0Hash9, depth0Hash10);
+            var depth1Hash6 = DataCalculatorNew.PairHashes(depth0Hash11, depth0Hash12);
+            var depth1Hash7 = DataCalculatorNew.PairHashes(depth0Hash13, depth0Hash14);
+            var depth1Hash8 = DataCalculatorNew.PairHashes(depth0Hash15, depth0Hash15);
+
+            var depth2Hash1 = DataCalculatorNew.PairHashes(depth1Hash1, depth1Hash2);
+            var depth2Hash2 = DataCalculatorNew.PairHashes(depth1Hash3, depth1Hash4);
+            var depth2Hash3 = DataCalculatorNew.PairHashes(depth1Hash5, depth1Hash6);
+            var depth2Hash4 = DataCalculatorNew.PairHashes(depth1Hash7, depth1Hash8);
+
+            var depth3Hash1 = DataCalculatorNew.PairHashes(depth2Hash1, depth2Hash2);
+            var depth3Hash2 = DataCalculatorNew.PairHashes(depth2Hash3, depth2Hash4);
+
+            var merkleRoot = DataCalculatorNew.PairHashes(depth3Hash1, depth3Hash2);
+
+            var blockElements = new List<BlockElement> { 
+                new BlockElement(blockHash, index: 0, depth: 0, hash: depth0Hash1),
+                new BlockElement(blockHash, index: 1, depth: 0, hash: depth0Hash2),
+                new BlockElement(blockHash, index: 2, depth: 0, hash: depth0Hash3),
+                new BlockElement(blockHash, index: 3, depth: 0, hash: depth0Hash4),
+                        new BlockElement(blockHash, index: 4, depth: 2, hash: depth2Hash2),
+                new BlockElement(blockHash, index: 8, depth: 0, hash: depth0Hash9),
+                new BlockElement(blockHash, index: 9, depth: 0, hash: depth0Hash10),
+                    new BlockElement(blockHash, index: 10, depth: 1, hash: depth1Hash6),
+                new BlockElement(blockHash, index: 12, depth: 0, hash: depth0Hash13),
+                new BlockElement(blockHash, index: 13, depth: 0, hash: depth0Hash14),
+                    new BlockElement(blockHash, index: 14, depth: 1, hash: depth1Hash8),
+            };
+
+            var readElements = DataCalculatorNew.ReadBlockElements(blockHash, merkleRoot, blockElements).ToList();
 
             CollectionAssert.AreEqual(blockElements, readElements);
         }
--- BitSharp.Core/DataCalculatorNew.cs
+++ BitSharp.Core/DataCalculatorNew.cs
@@ -1,8 +1,11 @@
 ﻿using BitSharp.Common;
+using BitSharp.Common.ExtensionMethods;
 using BitSharp.Core.Domain;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,11 +14,142 @@ namespace BitSharp.Core
     //TODO organize and name properly
     public static class DataCalculatorNew
     {
-        public static IEnumerable<BlockElement> ReadBlockElements(UInt256 merkleRoot, IEnumerable<BlockElement> blockElements)
+        public static IEnumerable<BlockElement> ReadBlockElements(UInt256 blockHash, UInt256 merkleRoot, IEnumerable<BlockElement> blockElements)
         {
+            var expectedIndex = 0;
+
+            var merkleStream = new MerkleStream();
+
             foreach (var blockElement in blockElements)
             {
+                if (blockElement.Index != expectedIndex)
+                {
+                    throw new ValidationException(blockHash);
+                }
+
+                merkleStream.AddHash(new MerkleHash(blockElement));
+
                 yield return blockElement;
+
+                expectedIndex += 1 << blockElement.Depth;
+            }
+
+            merkleStream.FinishPairing();
+
+            if (merkleStream.RootHash.Hash != merkleRoot)
+            {
+                throw new ValidationException(blockHash);
+            }
+        }
+
+        public static UInt256 PairHashes(UInt256 left, UInt256 right)
+        {
+            var bytes = ImmutableArray.CreateBuilder<byte>(64);
+            bytes.AddRange(left.ToByteArray());
+            bytes.AddRange(right.ToByteArray());
+            return new UInt256(new SHA256Managed().ComputeDoubleHash(bytes.ToArray()));
+        }
+
+        private class MerkleStream
+        {
+            private readonly List<MerkleHash> leftHashes = new List<MerkleHash>();
+
+            public MerkleHash RootHash
+            {
+                get
+                {
+                    if (this.leftHashes.Count != 1)
+                        throw new InvalidOperationException();
+
+                    return this.leftHashes[0];
+                }
+            }
+
+            public void AddHash(MerkleHash newHash)
+            {
+                if (this.leftHashes.Count == 0)
+                {
+                    this.leftHashes.Add(newHash);
+                }
+                else
+                {
+                    var leftHash = this.leftHashes.Last();
+
+                    if (newHash.Depth < leftHash.Depth)
+                    {
+                        this.leftHashes.Add(newHash);
+                    }
+                    else if (newHash.Depth == leftHash.Depth)
+                    {
+                        this.leftHashes[this.leftHashes.Count - 1] = leftHash.PairWith(newHash);
+                    }
+                    else if (newHash.Depth > leftHash.Depth)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    this.ClosePairs();
+                }
+            }
+
+            public void FinishPairing()
+            {
+                if (this.leftHashes.Count == 0)
+                    throw new InvalidOperationException();
+
+                while (this.leftHashes.Count > 1)
+                {
+                    AddHash(this.leftHashes.Last());
+                }
+            }
+
+            private void ClosePairs()
+            {
+                while (this.leftHashes.Count >= 2)
+                {
+                    var leftHash = this.leftHashes[this.leftHashes.Count - 2];
+                    var rightHash = this.leftHashes[this.leftHashes.Count - 1];
+
+                    if (leftHash.Depth == rightHash.Depth)
+                    {
+                        this.leftHashes.RemoveAt(this.leftHashes.Count - 1);
+                        this.leftHashes[this.leftHashes.Count - 1] = leftHash.PairWith(rightHash);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private struct MerkleHash
+        {
+            private readonly int depth;
+            private readonly UInt256 hash;
+
+            public MerkleHash(int depth, UInt256 hash)
+            {
+                this.depth = depth;
+                this.hash = hash;
+            }
+
+            public MerkleHash(BlockElement blockElement)
+            {
+                this.depth = blockElement.Depth;
+                this.hash = blockElement.Hash;
+            }
+
+            public int Depth { get { return this.depth; } }
+
+            public UInt256 Hash { get { return this.hash; } }
+
+            public MerkleHash PairWith(MerkleHash other)
+            {
+                if (this.depth != other.depth)
+                    throw new InvalidOperationException();
+
+                return new MerkleHash(this.depth + 1, PairHashes(this.hash, other.hash));
             }
         }
     }
