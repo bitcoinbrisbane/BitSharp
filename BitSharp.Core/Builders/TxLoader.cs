45d23bd52471879525d8e0b9bd2f57fa20244f10
Paul < pmlyon@hotmail.ca>
2014-07-07 14:38:55 -0300

    cache transactions for each block during loading

--- BitSharp.Core/Builders/TxLoader.cs
+++ BitSharp.Core/Builders/TxLoader.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,12 @@ namespace BitSharp.Core.Builders
         private readonly IBlockStorageNew blockCache;
         private readonly TxValidator txValidator;
         private readonly IBlockchainRules rules;
+        private readonly Logger logger;
+
+        private ConcurrentDictionary<UInt256, Transaction> txCache;
         private ConcurrentBag<Exception> exceptions;
+        private int txCount;
+        private int inputCount;
 
         public TxLoader(IBlockStorageNew blockCache, TxValidator txValidator, Logger logger, IBlockchainRules rules, bool isConcurrent)
             : base("TxLoader", isConcurrent, logger: logger)
@@ -28,6 +34,7 @@ namespace BitSharp.Core.Builders
             this.blockCache = blockCache;
             this.txValidator = txValidator;
             this.rules = rules;
+            this.logger = logger;
         }
 
         public ConcurrentBag<Exception> Exceptions
@@ -35,9 +42,22 @@ namespace BitSharp.Core.Builders
             get { return this.exceptions; }
         }
 
+        public int TxCount
+        {
+            get { return this.txCount; }
+        }
+
+        public int InputCount
+        {
+            get { return this.inputCount; }
+        }
+
         protected override void SubStart()
         {
+            this.txCache = new ConcurrentDictionary<UInt256, Transaction>();
             this.exceptions = new ConcurrentBag<Exception>();
+            this.txCount = 0;
+            this.inputCount = 0;
         }
 
         protected override void ConsumeItem(PendingTx pendingTx)
@@ -46,6 +66,8 @@ namespace BitSharp.Core.Builders
             {
                 if (MainnetRules.BypassValidation)
                     return;
+
+                this.txCount++;
 
                 var txIndex = pendingTx.txIndex;
                 var transaction = pendingTx.transaction;
@@ -58,30 +80,46 @@ namespace BitSharp.Core.Builders
                 // load previous transactions for each input, unless this is a coinbase transaction
                 if (!isCoinbase)
                 {
+                    this.inputCount += transaction.Inputs.Length;
+
                     for (var inputIndex = 0; inputIndex < transaction.Inputs.Length; inputIndex++)
                     {
                         var input = transaction.Inputs[inputIndex];
-                        var spentTx = spentTxes[inputIndex];
 
-                        Transaction prevTx;
-                        if (this.blockCache.TryGetTransaction(spentTx.blockHash, spentTx.txIndex, out prevTx))
+                        Transaction cachedPrevTx;
+                        if (this.txCache.TryGetValue(input.PreviousTxOutputKey.TxHash, out cachedPrevTx))
                         {
-                            if (input.PreviousTxOutputKey.TxHash != prevTx.Hash)
-                                throw new Exception("TODO");
-
-                            var prevTxOutput = prevTx.Outputs[input.PreviousTxOutputKey.TxOutputIndex.ToIntChecked()];
+                            var prevTxOutput = cachedPrevTx.Outputs[input.PreviousTxOutputKey.TxOutputIndex.ToIntChecked()];
                             prevTxOutputs.Add(prevTxOutput);
                         }
                         else
                         {
-                            throw new Exception("TODO");
+                            var spentTx = spentTxes[inputIndex];
+
+                            Transaction prevTx;
+                            if (this.blockCache.TryGetTransaction(spentTx.blockHash, spentTx.txIndex, out prevTx))
+                            {
+                                if (input.PreviousTxOutputKey.TxHash != prevTx.Hash)
+                                    throw new Exception("TODO");
+
+                                this.txCache.TryAdd(prevTx.Hash, prevTx);
+
+                                var prevTxOutput = prevTx.Outputs[input.PreviousTxOutputKey.TxOutputIndex.ToIntChecked()];
+                                prevTxOutputs.Add(prevTxOutput);
+                            }
+                            else
+                            {
+                                throw new Exception("TODO");
+                            }
                         }
                     }
+
+                    Debug.Assert(prevTxOutputs.Count == transaction.Inputs.Length);
                 }
 
                 if (!MainnetRules.IgnoreScripts)
                 {
-                    var pendingTxValid = new PendingTxValid(txIndex, transaction, chainedHeader, isCoinbase, prevTxOutputs.ToImmutable());
+                    var pendingTxValid = new PendingTxValid(txIndex, transaction, chainedHeader, isCoinbase, prevTxOutputs.ToImmutableArray());
                     this.txValidator.Add(pendingTxValid);
                 }
             }
@@ -93,6 +131,7 @@ namespace BitSharp.Core.Builders
 
         protected override void CompletedItems()
         {
+            this.txCache = null;
             this.txValidator.CompleteAdding();
         }
     }
