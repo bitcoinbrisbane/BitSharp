ec17f3e43d5b91b0b50cabb981090fd11ec751ec
Paul < pmlyon@hotmail.ca>
2014-07-11 20:59:02 -0300

    split storage provider test into infrastructure class and actual test, setup stubs for testing the storage interfaces

---
+++ BitSharp.Core.Test.Storage/IBlockTxesStorageTest.cs
@@ -0,0 +1,33 @@
+﻿using BitSharp.Common;
+using BitSharp.Common.ExtensionMethods;
+using BitSharp.Core;
+using BitSharp.Core.Builders;
+using BitSharp.Core.Domain;
+using BitSharp.Core.Rules;
+using BitSharp.Core.Storage;
+using BitSharp.Core.Test;
+using BitSharp.Core.Test.Rules;
+using BitSharp.Domain;
+using BitSharp.Esent.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Core.Test.Storage
+{
+    [TestClass]
+    public class IBlockTxesStorageTest : StorageProviderTest
+    {
+        [TestMethod]
+        public void TODO() { Assert.Fail("TODO"); }
+    }
+}
