2827f19b8eb32574c9ba9e271bb245da8f1f873a
Paul < pmlyon@hotmail.ca>
2014-07-12 13:36:18 -0300

    prepping IBlockStorage tests

--- BitSharp.Core.Test.Storage/IBlockStorageTest.cs
+++ BitSharp.Core.Test.Storage/IBlockStorageTest.cs
@@ -28,6 +28,80 @@ namespace BitSharp.Core.Test.Storage
     public class IBlockStorageTest : StorageProviderTest
     {
         [TestMethod]
-        public void TODO() { Assert.Inconclusive("TODO"); }
+        public void TestContainsChainedHeader()
+        {
+            RunTest(TestContainsChainedHeader);
+        }
+
+        [TestMethod]
+        public void TestTryAddChainedHeader()
+        {
+            RunTest(TestTryAddChainedHeader);
+        }
+
+        [TestMethod]
+        public void TestTryGetChainedHeader()
+        {
+            RunTest(TestTryGetChainedHeader);
+        }
+
+        [TestMethod]
+        public void TestFindMaxTotalWork()
+        {
+            RunTest(TestFindMaxTotalWork);
+        }
+
+        [TestMethod]
+        public void TestReadChainedHeaders()
+        {
+            RunTest(TestReadChainedHeaders);
+        }
+
+        [TestMethod]
+        public void TestIsBlockInvalid()
+        {
+            RunTest(TestIsBlockInvalid);
+        }
+
+        [TestMethod]
+        public void TestMarkBlockInvalid()
+        {
+            RunTest(TestMarkBlockInvalid);
+        }
+
+        private void TestContainsChainedHeader(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestTryAddChainedHeader(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestTryGetChainedHeader(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestFindMaxTotalWork(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestReadChainedHeaders(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestIsBlockInvalid(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestMarkBlockInvalid(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
     }
 }
