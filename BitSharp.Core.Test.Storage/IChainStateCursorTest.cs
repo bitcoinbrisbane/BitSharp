44bab049c2dc858bf732e3d03d0c572635d04d42
Paul < pmlyon@hotmail.ca>
2014-08-02 21:19:11 -0300

    stub out tests

--- BitSharp.Core.Test.Storage/IChainStateCursorTest.cs
+++ BitSharp.Core.Test.Storage/IChainStateCursorTest.cs
@@ -28,6 +28,135 @@ namespace BitSharp.Core.Test.Storage
     public class IChainStateCursorTest : StorageProviderTest
     {
         [TestMethod]
-        public void TODO() { Assert.Inconclusive("TODO"); }
+        public void TestBeginTransaction()
+        {
+            RunTest(TestBeginTransaction);
+        }
+
+        [TestMethod]
+        public void TestCommitTransaction()
+        {
+            RunTest(TestCommitTransaction);
+        }
+
+        [TestMethod]
+        public void TestRollbackTransaction()
+        {
+            RunTest(TestRollbackTransaction);
+        }
+
+        [TestMethod]
+        public void TestReadChain()
+        {
+            RunTest(TestReadChain);
+        }
+
+        [TestMethod]
+        public void TestAddChainedHeader()
+        {
+            RunTest(TestAddChainedHeader);
+        }
+
+        [TestMethod]
+        public void TestRemoveChainedHeader()
+        {
+            RunTest(TestRemoveChainedHeader);
+        }
+
+        [TestMethod]
+        public void TestUnspentTxCount()
+        {
+            RunTest(TestUnspentTxCount);
+        }
+
+        [TestMethod]
+        public void TestConainsUnspentTx()
+        {
+            RunTest(TestConainsUnspentTx);
+        }
+
+        [TestMethod]
+        public void TestTryGetUnspentTx()
+        {
+            RunTest(TestTryGetUnspentTx);
+        }
+
+        [TestMethod]
+        public void TestTryAddUnspentTx()
+        {
+            RunTest(TestTryAddUnspentTx);
+        }
+
+        [TestMethod]
+        public void TestTryRemoveUnspentTx()
+        {
+            RunTest(TestTryRemoveUnspentTx);
+        }
+
+        [TestMethod]
+        public void TestTryUpdateUnspentTx()
+        {
+            RunTest(TestTryUpdateUnspentTx);
+        }
+
+        private void TestBeginTransaction(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestCommitTransaction(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestRollbackTransaction(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestReadChain(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestAddChainedHeader(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestRemoveChainedHeader(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestUnspentTxCount(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestConainsUnspentTx(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestTryGetUnspentTx(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestTryAddUnspentTx(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestTryRemoveUnspentTx(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
+
+        private void TestTryUpdateUnspentTx(ITestStorageProvider provider)
+        {
+            Assert.Inconclusive("TODO");
+        }
     }
 }
