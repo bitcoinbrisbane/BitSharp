ca06a9c767ff4cd9dde2510770c080f47fa7b5fd
Paul < pmlyon@hotmail.ca>
2014-08-10 11:06:20 -0300

    update pull tester jar to use my repo with a fix for it hanging

--- BitSharp.IntegrationTest/PullTesterJar.ps1
+++ BitSharp.IntegrationTest/PullTesterJar.ps1
@@ -3,11 +3,13 @@ if (Test-Path bin) { rmdir -force -recurse bin }
 
 if (Test-Path bitcoinj)
 {
-	git --git-dir=bitcoinj/.git pull
+	pushd bitcoinj
+	git pull
+	popd
 }
 else
 {
-	git clone https://github.com/bitcoinj/bitcoinj.git bitcoinj
+	git clone https://github.com/pmlyon/bitcoinj.git bitcoinj
 }
 
 mvn -f bitcoinj/pom.xml -DskipTests clean package
