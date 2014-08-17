7175bd4494cdd457e40051749df1a9de7c774e58
Paul < pmlyon@hotmail.ca>
2014-08-17 19:54:30 -0300

    organize and comment ParallelConsumer, add unit tests

---
+++ BitSharp.Common.Test/ParallelConsumerTest.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.Common.Test
+{
+    [TestClass]
+    public class ParallelConsumerTest
+    {
+        [TestMethod]
+        public void TestConsume()
+        {
+            using (var consumer = new ParallelConsumer<int>("", 4, LogManager.CreateNullLogger()))
+            {
+                // create a source enumerable
+                var source = Enumerable.Range(0, 5).Select(x => x);
+
+                // create a consume action
+                var expectedResults = Enumerable.Range(0, 5).Select(x => x * 10).ToList();
+                var results = new ConcurrentBag<int>();
+                Action<int> consumeAction = x =>
+                    {
+                        results.Add(x * 10);
+                    };
+
+                // start consuming the source
+                using (consumer.Start(source, consumeAction, () => { }))
+                {
+                    // wait for the consumer to complete
+                    consumer.WaitToComplete();
+
+                    // verify results
+                    CollectionAssert.AreEquivalent(expectedResults, results);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestCompletedAction()
+        {
+            using (var consumer = new ParallelConsumer<int>("", 4, LogManager.CreateNullLogger()))
+            {
+                // create a source enumerable
+                var source = Enumerable.Range(0, 5).Select(x => x);
+
+                // create a completed action
+                var wasCompleted = false;
+                Action completedAction = () => wasCompleted = true;
+
+                // start consuming the source
+                using (consumer.Start(source, _ => { }, completedAction))
+                {
+                    // wait for the consumer to complete
+                    consumer.WaitToComplete();
+
+                    // verify completed action was called
+                    Assert.IsTrue(wasCompleted);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestSourceException()
+        {
+            using (var consumer = new ParallelConsumer<int>("", 4, LogManager.CreateNullLogger()))
+            {
+                // create a source enumerable that will throw an exception
+                var expectedException = new Exception();
+                var source = Enumerable.Range(0, 5).Select(
+                    x =>
+                    {
+                        if (x < 4)
+                            return x;
+                        else
+                            throw expectedException;
+                    });
+
+                // create a completed action
+                var wasCompleted = false;
+                Action completedAction = () => wasCompleted = true;
+
+                // start consuming the source
+                using (consumer.Start(source, _ => { }, completedAction))
+                {
+                    // wait for the consumer to complete, and verify the exception was bubbled up
+                    try
+                    {
+                        consumer.WaitToComplete();
+                        Assert.Fail("Expected exception was not thrown.");
+                    }
+                    catch (AggregateException e)
+                    {
+                        // verify expected exception
+                        Assert.AreEqual(1, e.InnerExceptions.Count);
+                        Assert.AreSame(expectedException, e.InnerExceptions[0]);
+
+                        // verify completed action was not called
+                        Assert.IsFalse(wasCompleted);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestConsumerException()
+        {
+            using (var consumer = new ParallelConsumer<int>("", 4, LogManager.CreateNullLogger()))
+            {
+                // create a source enumerable
+                var source = Enumerable.Range(0, 5).Select(x => x);
+
+                // create a consume action that will throw an exception on the last item
+                var expectedException = new Exception();
+                Action<int> consumeAction = x =>
+                    {
+                        if (x == 4)
+                            throw expectedException;
+                    };
+
+                // create a completed action
+                var wasCompleted = false;
+                Action completedAction = () => wasCompleted = true;
+
+                // start consuming the source
+                using (consumer.Start(source, consumeAction, completedAction))
+                {
+                    // wait for the consumer to complete, and verify the exception was bubbled up
+                    try
+                    {
+                        consumer.WaitToComplete();
+                        Assert.Fail("Expected exception was not thrown.");
+                    }
+                    catch (AggregateException e)
+                    {
+                        // verify expected exception
+                        Assert.AreEqual(1, e.InnerExceptions.Count);
+                        Assert.AreSame(expectedException, e.InnerExceptions[0]);
+
+                        // verify completed action was not called
+                        Assert.IsFalse(wasCompleted);
+                    }
+                }
+            }
+        }
+    }
+}
--- BitSharp.Common/ParallelConsumer.cs
+++ BitSharp.Common/ParallelConsumer.cs
@@ -11,150 +11,242 @@ using System.Threading.Tasks;
 
 namespace BitSharp.Common
 {
+    /// <summary>
+    /// <para>ParalleConsumer provides a fixed thread pool for reading an enumerable source on one
+    /// thread, and queueing each item to be consumed on a separate threads.</para>
+    /// </summary>
+    /// <remarks>
+    /// <para>This provides very similar functionality to using a Parallel.ForEach to queue up work, without the overhead.</para>
+    /// <para>When making thousands of Parallel.ForEach calls per second, delays will be hit starting the tasks.</para>
+    /// </remarks>
+    /// <typeparam name="T">The type of the items to be read and consumed.</typeparam>
     public class ParallelConsumer<T> : IDisposable
     {
         private readonly string name;
         private readonly Logger logger;
 
+        // the worker thread where the source will be read
         private readonly WorkerMethod readWorker;
-        private ManualResetEventSlim completedReadingEvent = new ManualResetEventSlim(false);
 
+        // the pool of worker threads where the source items will be consumed
         private readonly WorkerMethod[] consumeWorkers;
-        private readonly Barrier consumeBarrier;
 
+        // event to track when reading and consuming have been completed
         private readonly ManualResetEventSlim completedEvent = new ManualResetEventSlim(false);
 
+        // the source enumerable to read
         private IEnumerable<T> source;
+
+        // the action to perform on each item read from the source
         private Action<T> consumeAction;
+
+        // an action to perform when all items have been read and consumed
         private Action completedAction;
+
+        // the queue of read items that  are waiting to be consumed
         private ConcurrentBlockingQueue<T> queue;
 
+        // track consumer work thread completions
+        private int consumeCompletedCount;
+
+        // any exceptions that occur during reading or consuming
+        private ConcurrentBag<Exception> exceptions;
+
+        // whether a consuming session is started
         private bool isStarted;
 
+        private bool isDisposed;
+
+        /// <summary>
+        /// Initialize a new ParallelConsumer with a fixed-sized pool of threads.
+        /// </summary>
+        /// <param name="name">The name of the instance.</param>
+        /// <param name="consumerThreadCount">The number of consumer threads to create.</param>
+        /// <param name="logger">A logger.</param>
         public ParallelConsumer(string name, int consumerThreadCount, Logger logger)
         {
             this.name = name;
             this.logger = logger;
 
+            // initialize the read worker
             this.readWorker = new WorkerMethod(name + ".ReadWorker", ReadWorker, initialNotify: false, minIdleTime: TimeSpan.Zero, maxIdleTime: TimeSpan.MaxValue, logger: logger);
             this.readWorker.Start();
 
+            // initialize a pool of consume workers
             this.consumeWorkers = new WorkerMethod[consumerThreadCount];
             for (var i = 0; i < this.consumeWorkers.Length; i++)
             {
                 this.consumeWorkers[i] = new WorkerMethod(name + ".ConsumeWorker." + i, ConsumeWorker, initialNotify: false, minIdleTime: TimeSpan.Zero, maxIdleTime: TimeSpan.MaxValue, logger: logger);
                 this.consumeWorkers[i].Start();
             }
-            
-            this.consumeBarrier = new Barrier(0,
-                b =>
-                {
-                    this.completedAction();
-                    this.completedEvent.Set();
-                });
         }
 
+        /// <summary>
+        /// Stop consuming and release all resources.
+        /// </summary>
         public void Dispose()
         {
-            this.Stop();
+            if (this.isDisposed)
+                return;
 
             this.consumeWorkers.DisposeList();
-
             new IDisposable[]
             {
                 this.readWorker,
-                this.completedReadingEvent,
                 this.completedEvent,
-                this.queue,
-                this.consumeBarrier
+                this.queue
             }.DisposeList();
+
+            this.isDisposed = true;
         }
 
+        /// <summary>
+        /// The name of the instance.
+        /// </summary>
         public string Name { get { return this.name; } }
 
+        /// <summary>
+        /// Start a new parallel consuming session on the provided source, calling the provided actions.
+        /// </summary>
+        /// <param name="source">The source enumerable to read from.</param>
+        /// <param name="consumeAction">The action to be called on each item from the source.</param>
+        /// <param name="completedAction">An action to be called when all items have been successfully read and consumed. This will not be called if there is an exception.</param>
+        /// <returns>An IDisposable to cleanup the parallel consuming session.</returns>
         public IDisposable Start(IEnumerable<T> source, Action<T> consumeAction, Action completedAction)
         {
             if (this.isStarted)
                 throw new InvalidOperationException();
 
+            // store the source enumerable and actions
             this.source = source;
             this.consumeAction = consumeAction;
             this.completedAction = completedAction;
-            this.queue = new ConcurrentBlockingQueue<T>();
 
-            this.completedReadingEvent.Reset();
+            // initialize queue and exceptions and reset completed count
+            this.queue = new ConcurrentBlockingQueue<T>();
+            this.exceptions = new ConcurrentBag<Exception>();
+            this.consumeCompletedCount = 0;
+
+            // set to the started state
             this.completedEvent.Reset();
             this.isStarted = true;
 
+            // notify the read worker to begin
             this.readWorker.NotifyWork();
-            
-            this.consumeBarrier.AddParticipants(this.consumeWorkers.Length);
-            for (var i = 0; i < this.consumeWorkers.Length; i++)
-            {
-                this.consumeWorkers[i].NotifyWork();
-            }
 
+            // notify the consume workers to begin
+            for (var i = 0; i < this.consumeWorkers.Length; i++)
+                this.consumeWorkers[i].NotifyWork();
+
+            // return the IDisposable to cleanup this session
             return new Stopper(this);
         }
 
+        /// <summary>
+        /// Blocks until all reading and consuming has been finished.
+        /// </summary>
+        /// <exception cref="AggregateException">Thrown if any exceptions occurred during reading or consuming. Contains the thrown exceptions.</exception>
         public void WaitToComplete()
         {
             if (!this.isStarted)
                 throw new InvalidOperationException();
 
-            this.completedReadingEvent.Wait();
+            // wait for reading and consuming to completed
             this.completedEvent.Wait();
+
+            // if any exceptions were thrown, rethrow them here
+            if (this.exceptions.Count > 0)
+                throw new AggregateException(this.exceptions);
         }
 
         private void Stop()
         {
             if (!this.isStarted)
-                return;
+                throw new InvalidOperationException();
 
-            this.WaitToComplete();
+            // wait for the completed state
+            this.isStarted = false;
+            this.completedEvent.Wait();
 
-            this.consumeBarrier.RemoveParticipants(this.consumeBarrier.ParticipantCount);
+            // dispose the queue
             this.queue.Dispose();
+
+            // null out all the fields used for this session
             this.source = null;
             this.consumeAction = null;
             this.completedAction = null;
             this.queue = null;
-
-            this.isStarted = false;
+            this.exceptions = null;
         }
 
         private void ReadWorker(WorkerMethod instance)
         {
+            // read all source items
             try
             {
                 foreach (var item in this.source)
                 {
+                    // break early if stopped or if an exception occurred
+                    if (!this.isStarted || this.exceptions.Count > 0)
+                        return;
+
+                    // queue the item to be consumed
                     this.queue.Add(item);
                 }
             }
+            // capture any thrown exceptions
+            catch (Exception e)
+            {
+                this.exceptions.Add(e);
+            }
+            // ensure queue is marked as complete for adding once reading has finished
             finally
             {
                 this.queue.CompleteAdding();
-                this.completedReadingEvent.Set();
             }
         }
 
         private void ConsumeWorker(WorkerMethod instance)
         {
+            // consume all read items
             try
             {
                 foreach (var value in this.queue.GetConsumingEnumerable())
+                {
+                    // break early if stopped or if an exception occurred
+                    if (!this.isStarted || this.exceptions.Count > 0)
+                        return;
+
+                    // consume the item
                     this.consumeAction(value);
+                }
             }
+            // capture any thrown exceptions
+            catch (Exception e)
+            {
+                this.exceptions.Add(e);
+            }
+            // ensure consumer thread completion is tracked
             finally
             {
-                this.consumeBarrier.SignalAndWait();
+                // increment the completed consumer count and check if all have been completed
+                if (Interlocked.Increment(ref this.consumeCompletedCount) == this.consumeWorkers.Length)
+                {
+                    // if no exceptions occurred, invoke the completed action
+                    if (this.exceptions.Count == 0)
+                        this.completedAction();
+
+                    // notify that reading and consuming have been completed
+                    this.completedEvent.Set();
+                }
             }
         }
 
+        // IDisposable to initiate cleanup of a consuming session
         private sealed class Stopper : IDisposable
         {
             private readonly ParallelConsumer<T> worker;
+            private bool isDisposed;
 
             public Stopper(ParallelConsumer<T> worker)
             {
@@ -163,7 +255,13 @@ namespace BitSharp.Common
 
             public void Dispose()
             {
+                if (this.isDisposed)
+                    return;
+
+                // stop the consuming session
                 this.worker.Stop();
+
+                this.isDisposed = true;
             }
         }
     }
