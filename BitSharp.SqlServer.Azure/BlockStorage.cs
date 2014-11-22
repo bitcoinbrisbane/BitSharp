1c2b0789b52a942dac00b97185fbc10f1f2750d2
Lucas Cullen <support@dugongsoftware.com>
2014-11-22 20:37:21 +1000

    Add sql azure and ef project

---
+++ BitSharp.SqlServer.Azure/BlockStorage.cs
@@ -0,0 +1,67 @@
+﻿using BitSharp.Core.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitSharp.SqlServer.Azure
+{
+    public class BlockStorage : IBlockStorage
+    {
+        public bool ContainsChainedHeader(Common.UInt256 blockHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryAddChainedHeader(Core.Domain.ChainedHeader chainedHeader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetChainedHeader(Common.UInt256 blockHash, out Core.Domain.ChainedHeader chainedHeader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryRemoveChainedHeader(Common.UInt256 blockHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Core.Domain.ChainedHeader FindMaxTotalWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Core.Domain.ChainedHeader> ReadChainedHeaders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsBlockInvalid(Common.UInt256 blockHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarkBlockInvalid(Common.UInt256 blockHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Defragment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
